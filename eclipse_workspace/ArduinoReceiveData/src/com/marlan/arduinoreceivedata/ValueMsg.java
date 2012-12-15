97a770f3a94ed5b80b7938f18e78f0ff621a6db4
rsm <rsm@Roberts-MacBook-Pro.local>
2012-12-14 23:15:35 -0800

    Android Arduino ADK LED samples

---
+++ eclipse_workspace/ArduinoReceiveData/src/com/marlan/arduinoreceivedata/ValueMsg.java
@@ -0,0 +1,17 @@
+    package com.marlan.arduinoreceivedata;
+    public class ValueMsg {
+    private char flag;
+    private int reading;
+     
+    public ValueMsg(char flag, int reading) {
+    this.flag = flag;
+    this.reading = reading;
+    }
+     
+    public int getReading() {
+    return reading;
+    }
+    public char getFlag() {
+    return flag;
+    }
+    }
